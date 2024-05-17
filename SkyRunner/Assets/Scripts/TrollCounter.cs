0b3a39288ac0b4663e1d6b44ba376e491f6e2c97
dofiHG <dofihgsafafafa@gmail.com>
2024-05-17 22:16:22 +0300

    1

--- SkyRunner/Assets/Scripts/TrollCounter.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TrollCounter : MonoBehaviour
-{
-    public float trollPerClick = 1;
-    public float trollPerSecond = 0;
-    public float countTrollNow = 0;
-    public int level = 1;
-}
