2e1ecf32b2b5d51ce2479efe414517cb9c298f86
dofiHG <dofihgsafafafa@gmail.com>
2024-06-20 22:56:16 +0300

    Lvl14

--- SkyRunner/Assets/Scripts/Character/CharacterStates.cs
+++ SkyRunner/Assets/Scripts/Character/CharacterStates.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 
 public class CharacterStates : MonoBehaviour
 {
-    
     [SerializeField] private GameObject _deathPanel;
+    [SerializeField] private GameObject _pausePanel;
     [SerializeField] private Slider _healtSlider;
     [SerializeField] private Slider _manaSlider;
 
@@ -17,6 +17,26 @@ public class CharacterStates : MonoBehaviour
     {
         _healtSlider.value = _hp;
         _manaSlider.value = _mana;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_deathPanel.GetComponent<Animator>().GetBool("Finish") != true && GameObject.FindGameObjectWithTag("FinishPanel").GetComponent<Animator>().GetBool("Finish") != true)
+            {
+                if (_pausePanel.GetComponent<Animator>().GetBool("Pause") != true)
+                {
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMover>().enabled = false;
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterAttack>().enabled = false;
+                    _pausePanel.GetComponent<Animator>().SetBool("Pause", true);
+                }
+                 
+                else
+                {
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMover>().enabled = true;
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterAttack>().enabled = true;
+                    _pausePanel.GetComponent<Animator>().SetBool("Pause", false);
+                }
+            }        
+        }
 
         if (_hp <= 0) 
         {
--- SkyRunner/Assets/Scripts/Enemies/DangerousLayer.cs
+++ SkyRunner/Assets/Scripts/Enemies/DangerousLayer.cs
@@ -4,7 +4,11 @@ public class DangerousLayer : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        try { collision.gameObject.GetComponent<CharacterStates>()._hp = 0; }
-        catch { collision.gameObject.GetComponent<EnemyStates>()._hp = 0; }
+        try 
+        { 
+            collision.gameObject.GetComponent<CharacterStates>()._hp = 0;
+            collision.gameObject.GetComponent<EnemyStates>()._hp = 0;
+        }
+        catch { }
     }
 }
