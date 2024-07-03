f7c819f35ecc8e8db95a8f978ac42cb1d7fa4364
dofiHG <dofihgsafafafa@gmail.com>
2024-07-03 09:41:18 +0300

    Finish

--- SkyRunner/Assets/Scripts/ChangeLangInPlayScene.cs
+++
@@ -1,9 +0,0 @@
-using Lean.Localization;
-using UnityEngine;
-
-public class ChangeLangInPlayScene : MonoBehaviour
-{
-    private string[] _languages = { "Rus", "Eng" };
-
-    private void Start() => LeanLocalization.SetCurrentLanguageAll(_languages[PlayerPrefs.GetInt("langInt")]);
-}
